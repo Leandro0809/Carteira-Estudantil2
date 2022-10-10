880799135261e5b356ec15c88a072f582685dffe
Leandro0809 <leoghoul599@gmail.com>
2022-10-10 15:31:24 -0300

    Adicionei a funcionalidade de usuario

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tvCadastrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.teste_main);
+        getSupportActionBar().hide();
+        tvCadastrar = findViewById((R.id.tvCadastro));
+        tvCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, login1.class);
+                startActivity(it);
+
+            }
+        });
     }
 }
